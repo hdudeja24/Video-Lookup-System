a559fdd5570b3138535898258baa7aa38abe770c
hdudeja24 <hitanshududeja80@gmail.com>
2020-11-07 17:19:13 -0500

    Create LinkedList.java
    Implemented an interface for the LL.

---
+++ src/LinkedList.java
@@ -0,0 +1,17 @@
+public interface LinkedList <Type> {
+    public boolean add(Type item);
+
+    public boolean isempty();
+
+    public void clear();
+
+    public Type get(int index);
+
+    public boolean contains(Object o);
+
+    public int indexOf(Object index);
+
+    public int size();
+
+    public void remove(int index);
+}
