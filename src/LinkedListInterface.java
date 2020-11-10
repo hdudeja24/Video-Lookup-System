d26ed6b76c9630db4e43af360f8d67260815f0f1
hdudeja24 <hitanshududeja80@gmail.com>
2020-11-10 18:24:32 -0500

    Refractored LLInterface and Class
    Added key, value in LL nodes for unique identifiers and comparision purposes.

--- src/LinkedListInterface.java
+++ src/LinkedListInterface.java
@@ -1,13 +1,13 @@
-public interface LinkedListInterface <Type> {
-    public boolean insert(Type item);
+public interface LinkedListInterface <Key,Value> {
+    public boolean insert(Key key, Value value);
 
     public boolean isEmpty();           // If the LL is empty return true, else return false
 
-    public boolean isFound(Type item);  // If the item is in the list
+    public boolean isFound(Key key);  // If the item is in the list
 
     public int size();                  // Return the current size of the LL
 
-    public boolean remove(Type item);   // If the item is in the LL, we remove and return true. Else item not in the LL return false as we can't remove
+    public Value remove(Key key);   // If the item is in the LL, we remove and return true. Else item not in the LL return false as we can't remove
 
     public void printLL();              // Iterate and print the values inside the LL
 }
--- src/Project3.java
+++ src/Project3.java
@@ -2,8 +2,7 @@ public class Project3 {
 
     public static void main(String [] arguments) {
 
-
-
+       
 
     }
 
